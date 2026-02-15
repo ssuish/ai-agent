0d8dfa3eaa39e234fd4ab704baf97795ba6a079a
ssuish <quidor.adrean@gmail.com>
2026-02-15 13:48:12 +0800

    Implement AI content generation using Google GenAI API; load environment variables for API key and handle response metadata.

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+client = genai.Client(api_key=api_key)
+model = "gemini-2.5-flash"
+contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+response = client.models.generate_content(model=model, contents=contents)
+
+print("User prompt:", contents)
+
+if response.usage_metadata:
+    prompt_token = response.usage_metadata.prompt_token_count
+    response_token = response.usage_metadata.candidates_token_count
+
+    print(f"Prompt tokens: {prompt_token}")
+    print(f"Response tokens: {response_token}")
+else:
+    raise RuntimeError("Usage metadata not found")
+
+print("Response:")
+print(response.text)
