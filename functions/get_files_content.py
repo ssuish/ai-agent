4ea2c158a874161d8701a33e916d392fe43ee0a0
ssuish <quidor.adrean@gmail.com>
2026-02-17 15:22:34 +0800

    Refactor error messages in get_file_content and write_file functions to use relative file paths instead of absolute paths for improved clarity and consistency.

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -7,16 +7,16 @@ def get_file_content(working_directory, file_path):
         file_path_abs = os.path.abspath(os.path.join(working_directory_abs, file_path))
         valid_target_dir = os.path.commonpath([working_directory_abs, file_path_abs]) == working_directory_abs
         if not valid_target_dir:
-            raise Exception(f'Cannot list "{file_path_abs}" as it is outside the permitted working directory')
+            raise Exception(f'Cannot list "{file_path}" as it is outside the permitted working directory')
 
         if not os.path.isfile(file_path_abs):
-            raise Exception(f'File not found or is not a regular file: "{file_path_abs}"')
+            raise Exception(f'File not found or is not a regular file: "{file_path}"')
 
         with open(file_path_abs, "r") as f:
             file_content = f.read(CHARACTER_LIMIT)
 
             if f.read(1):
-                file_content += f'[...File "{file_path_abs}" truncated at {CHARACTER_LIMIT} characters]'
+                file_content += f'[...File "{file_path}" truncated at {CHARACTER_LIMIT} characters]'
 
             return file_content
     except Exception as e:
